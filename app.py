37c962acee24f9bf4217efec5292c873253e7301
Radhika-15-Agarwal <24ucs128@lnmiit.ac.in>
2026-06-08 14:36:53 +0530

    Add Chroma vector store with local embeddings

--- app.py
+++ app.py
@@ -1,5 +1,14 @@
-from rag import load_pdfs
+from rag import (
+    load_pdfs,
+    split_documents,
+    create_vector_store
+)
 
 docs = load_pdfs()
+chunks = split_documents(docs)
 
-print(f"Loaded {len(docs)} pages")
+print(f"Creating embeddings for {len(chunks)} chunks...")
+
+vector_store = create_vector_store(chunks)
+
+print("Vector store created successfully!")
--- rag.py
+++ rag.py
@@ -1,6 +1,12 @@
 from pathlib import Path
-from langchain_community.document_loaders import PyPDFLoader
 
+from dotenv import load_dotenv
+load_dotenv()
+
+from langchain_community.document_loaders import PyPDFLoader
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_community.embeddings import HuggingFaceEmbeddings
+from langchain_community.vectorstores import Chroma
 
 def load_pdfs(folder_path="data"):
     documents = []
@@ -16,3 +22,27 @@ def load_pdfs(folder_path="data"):
         documents.extend(loader.load())
 
     return documents
+
+
+def split_documents(documents):
+
+    splitter = RecursiveCharacterTextSplitter(
+        chunk_size=1000,
+        chunk_overlap=200
+    )
+
+    return splitter.split_documents(documents)
+
+def create_vector_store(chunks):
+
+    embeddings = HuggingFaceEmbeddings(
+        model_name="sentence-transformers/all-MiniLM-L6-v2"
+    )
+
+    vector_store = Chroma.from_documents(
+        documents=chunks,
+        embedding=embeddings,
+        persist_directory="./chroma_db"
+    )
+
+    return vector_store
